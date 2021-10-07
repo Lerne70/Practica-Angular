8772e0866016d67514b13a5301404df2b53b21ea
Lerne70 <56684878+Lerne70@users.noreply.github.com>
2021-10-07 17:09:33 -0500

    Practica de Angular - Inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador.component';
+import { HereoComponent } from './heroes/hereo/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContadorComponent,
+    HereoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/heroes/hereo/heroe.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-heroe',
+    templateUrl: './heroe.component.html'
+})
+export class HereoComponent {
+
+}
